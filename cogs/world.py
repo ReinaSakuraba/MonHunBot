d0a9844f4e7bfe3b1a99665e0a76a8f6602d38d6
ReinaSakuraba <juniorterra7@gmail.com>
2018-03-17 10:06:59 +0000

    Create motion value command

--- cogs/world.py
+++ cogs/world.py
@@ -14,6 +14,9 @@ class World:
         with open('mhw/charms.json') as f:
             self.charms = json.load(f)
 
+        with open('mhw/motionvalues.json') as f:
+            self.motion_values = json.load(f)
+
     @commands.command()
     async def charm(self, ctx, *, name: str.lower):
         match = self.charm_re.match(name)
@@ -39,6 +42,74 @@ class World:
             embed.add_field(name='Materials', value=mats, inline=False)
 
         await ctx.send(embed=embed)
+
+    @commands.command()
+    async def mv(self, ctx, *, weapon: str.lower):
+        transformations = {
+            'gs': 'great sword',
+            'great sword': 'great sword',
+            'greatsword': 'great sword',
+            'ls': 'long sword',
+            'long sword': 'long sword',
+            'longsword': 'long sword',
+            'sns': 'sword and shield',
+            'sword and shield': 'sword and shield',
+            'sword n shield': 'sword and shield',
+            'sword \'n\' shield': 'sword and shield',
+            'dbs': 'dual blades',
+            'dual blades': 'dual blades',
+            'dualblades': 'dual blades',
+            'dual blade': 'dual blades',
+            'dual blades': 'dual blades',
+            'duals': 'dual blades',
+            'hammer': 'hammer',
+            'hh': 'hunting horn',
+            'hunting horn': 'hunting horn',
+            'huntinghorn': 'hunting horn',
+            'lance': 'lance',
+            'gl': 'gunlance',
+            'gunlance': 'gunlance',
+            'gun lance': 'gunlance',
+            'sa': 'switch axe',
+            'switch axe': 'switch axe',
+            'switchaxe': 'switch axe',
+            'cb': 'charge blade',
+            'charge blade': 'charge blade',
+            'chargeblade': 'charge blade',
+            'ig': 'insect glaive',
+            'insect glaive': 'insect glaive',
+            'insectglaive': 'insect glaive',
+            'lbg': 'light bowgun',
+            'light bowgun': 'light bowgun',
+            'light bow gun': 'light bowgun',
+            'lightbow gun': 'light bowgun',
+            'lighbowgun': 'light bowgun',
+            'hbg': 'heavy bowgun',
+            'heavy bowgun': 'heavy bowgun',
+            'heavy bow gun': 'heavy bowgun',
+            'heavybow gun': 'heavy bowgun',
+            'heavybowgun': 'heavy bowgun',
+            'bow': 'bow'
+        }
+
+        weapon = transformations.get(weapon)
+        if weapon is None:
+            return await ctx.send('Weapon not found.')
+
+        weapon_values = self.motion_values[weapon]
+
+        table = utils.TabularData()
+        table.set_columns(['Move', 'Damage Type', 'Motion Value/Stun/Exhaust'])
+        for move, data in weapon_values.items():
+            table.add_row([move, data['Damage Type'], f"{data['Motion Value']}/{data['Stun']}/{data['Exhaust']}"])
+
+        render = table.render()
+        paginator = commands.Paginator()
+        for line in render.split('\n'):
+            paginator.add_line(line)
+
+        for p in paginator.pages:
+            await ctx.send(p)
 
 
 def setup(bot):
