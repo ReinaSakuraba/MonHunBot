eec693c5a16dce89b90fed0e641fb78858166902
ReinaSakuraba <juniorterra7@gmail.com>
2018-03-28 13:27:21 +0000

    Add armor reference to armor skills

--- launcher.py
+++ launcher.py
@@ -40,7 +40,7 @@ async def create_db(pool):
             );
 
             CREATE TABLE IF NOT EXISTS world.armor_skills (
-                name TEXT,
+                name TEXT REFERENCES world.armor(name),
                 skill TEXT REFERENCES world.skills(name),
                 level SMALLINT NOT NULL,
                 PRIMARY KEY(name, skill)
